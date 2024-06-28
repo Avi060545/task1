dd446d982812a6cd9adef00a808a8ffc8e4cc1c9
Avi060545 <avi@betalectic.com>
2024-06-28 11:23:32 +0530

    third

---
+++ src/components/Btn.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+ function Btn ()
+ {
+
+    return (
+        <div>
+
+            <button onClick={console.log("hello")}>
+
+            </button>
+        </div>
+    )
+ }
